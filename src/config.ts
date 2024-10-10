69577174352e74db3fac7a84ddb533655aba4602
thick-milk <147240734+thick-milk@users.noreply.github.com>
2024-10-10 11:19:47 +0800

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,106 @@
+import type { Site, Ui, Features } from './types'
+
+export const SITE: Site = {
+  website: 'https://astro-antfustyle-theme.vercel.app/',
+  base: '/',
+  title: 'Astro AntfuStyle Theme',
+  description:
+    'A customizable, feature-rich Astro theme for blog and portfolio creation',
+  author: 'Stephanie Lin',
+  lang: 'en',
+  ogLocale: 'en_US',
+}
+
+export const UI: Ui = {
+  internalNavs: [
+    {
+      path: '/blog',
+      title: 'Blog',
+      displayMode: 'textToIconOnMobile',
+      text: 'Blog',
+      icon: 'i-ri-article-line',
+    },
+    {
+      path: '/projects',
+      title: 'Projects',
+      displayMode: 'textToIconOnMobile',
+      text: 'Projects',
+      icon: 'i-ri-lightbulb-line',
+    },
+    {
+      path: '/changelog',
+      title: 'Changelog',
+      displayMode: 'iconHiddenOnMobile',
+      icon: 'i-ri-draft-line',
+    },
+  ],
+  socialLinks: [
+    {
+      link: 'https://github.com/lin-stephanie/astro-antfustyle-theme',
+      title: 'AntfuStyle on Github',
+      displayMode: 'alwaysIcon',
+      icon: 'i-uil-github-alt',
+    },
+    {
+      link: 'https://x.com/ste7lin',
+      title: `${SITE.author} on Twitter`,
+      displayMode: 'iconHiddenOnMobile',
+      icon: 'i-ri-twitter-x-fill',
+    },
+  ],
+  navBarLayout: {
+    left: [],
+    right: [
+      'internalNavs',
+      'socialLinks',
+      'searchButton',
+      'themeButton',
+      'rssLink',
+    ],
+  },
+  tabbedLayoutTabs: [
+    { title: 'Changelog', path: '/changelog' },
+    { title: 'AstroBlog', path: '/feeds' },
+    { title: 'AstroStreams', path: '/streams' },
+  ],
+  maxGroupColumns: 3,
+  showGroupItemColorOnHover: true,
+}
+
+/**
+ * Configures whether to enable special features:
+ *  - Set to `false` or `[false, {...}]` to disable the feature.
+ *  - Set to `[true, {...}]` to enable and configure the feature.
+ */
+export const FEATURES: Features = {
+  share: [
+    true,
+    {
+      twitter: [true, '@ste7lin'],
+      mastodon: [true, '@ste7lin@fairy.id'],
+      facebook: false,
+      pinterest: false,
+      reddit: false,
+      telegram: false,
+      whatsapp: false,
+      email: false,
+    },
+  ],
+  toc: [
+    true,
+    {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 4,
+      displayPosition: 'left',
+      displayMode: 'hover',
+    },
+  ],
+  ogImage: [
+    true,
+    {
+      authorOrBrand: `${SITE.title}`,
+      fallbackTitle: `${SITE.description}`,
+      fallbackBgType: 'plum',
+    },
+  ],
+}
